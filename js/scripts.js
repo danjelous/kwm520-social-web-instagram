af45912aeecd6c540a886d4f8c201152cc486ef1
Fabian Dopler <fabidop@gmail.com>
2017-06-29 21:12:28 +0200

    vison + faceapi test

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// --------------------------------------------------------------------------------------------------------
+// Zwischenablage
+// https://westus.dev.cognitive.microsoft.com/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f30395236
+// https://azure.microsoft.com/en-us/try/cognitive-services/my-apis/
+// --------------------------------------------------------------------------------------------------------
+
 // Get userID from username:
 // https://www.instagram.com/{username}/?__a=1
 
@@ -13,7 +19,100 @@ var feed = new Instafeed({
     get: 'user',
     userId: '5663183675',
     accessToken: '5663183675.6bcdfd3.4faacb05423041ea9e2a80e4c873b867',
-    template: '<h2 style="margin-top: 30px">{{caption}}</h2><a href="{{link}}"><img src="{{image}}" /></a><a href="{{image}}" target="_blank" style="display: block;">Image source: {{image}}</a><hr>',
+    template: '<div class="item"><h2 style="margin-top: 30px">{{caption}}</h2><a href="{{link}}"><img src="{{image}}" /></a><a href="{{image}}" target="_blank" style="display: block;">Image source: {{image}}</a><hr></div>',
     resolution: 'low_resolution'
 });
 feed.run();
+
+// Vision API
+// send to microsoft api
+
+function sendToMicrosoftVision (pictureLink) {
+    $.ajax({
+            url: "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories,Tags,Description,Faces,ImageType,Color,Adult",
+            beforeSend: function (xhrObj) {
+                // Request headers
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                //xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "1e040e58d4294bc6b6b075816d863a13");
+            },
+            type: "POST",
+            crossDomain: true,
+            processData: false,
+            //dataType: 'jsonp',
+            // Request body
+            //data: imgStream,
+            data: '{ "url": "'+ pictureLink +'" }'
+        })
+        .done(function (data) {
+            console.log("##### WEBREQUEST SUCCESS: RESPONSE: #####");
+            console.log(data);
+            //$('#overlay').css('display','none');
+            //draw(data);
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("##### WEBREQUEST FAILED : Output: #####");
+            console.log(textStatus);
+            //$('#overlay').css('display','none');
+        });
+}
+
+// Face API
+// send to microsoft api
+
+function sendToMicrosoftFace (pictureLink) {
+    $.ajax({
+            url: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure",
+            beforeSend: function (xhrObj) {
+                // Request headers
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                //xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "dc73d0ae908646e4b863b2ca0f329ef5");
+            },
+            type: "POST",
+            crossDomain: true,
+            processData: false,
+            //dataType: 'jsonp',
+            // Request body
+            //data: imgStream,
+            data: '{ "url": "'+ pictureLink +'" }'
+        })
+        .done(function (data) {
+            console.log("##### WEBREQUEST SUCCESS: RESPONSE: #####");
+            console.log(data);
+            //$('#overlay').css('display','none');
+            //draw(data);
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("##### WEBREQUEST FAILED : Output: #####");
+            console.log(textStatus);
+            //$('#overlay').css('display','none');
+        });
+}
+
+
+//draw img @postion from response
+function draw (data) {
+    for (var i = 0; i<data.length; i++) {
+        var face = $("<img>").attr('src', returnImgString(data[i])).attr('class', 'emoji').css(
+            {
+                "left": data[i].faceRectangle.left/scaleFactor(),
+                "top": data[i].faceRectangle.top/scaleFactor(),
+                "width" : data[i].faceRectangle.width/scaleFactor()
+            });
+        $('#result').append(face);
+        console.log ("scale = " + scaleFactor())
+    }
+}
+
+//return Img String to use
+function returnImgString (person) {
+    return 'img/'+returnEmotion(person.scores)+'.png';
+}
+
+//return closest emotion value
+function returnEmotion (scores) {
+    var max = Math.max.apply(null,Object.keys(scores).map(function(x){ return scores[x] }));
+    var emotion = Object.keys(scores).filter(function(x){ return scores[x] == max; })[0];
+    return emotion;
+}
